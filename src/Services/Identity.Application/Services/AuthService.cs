8dcc60e0d4b55fe90fcfc5e14aeab1df2e2dbcb1
Rafael Monteiro Porto <rafael.monteiro@easynvest.com.br>
2020-11-16 22:21:13 -0300

    Criado gitignore

---
+++ src/Services/Identity.Application/Services/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Identity.Core;
+using Identity.Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Application.Services
+{
+	public class AuthService
+	{
+		private readonly UserManager<User> _userManager;
+		private readonly SignInManager<User> _signInManager;
+		public AuthService(UserManager<User> userManager, SignInManager<User> signInManager) =>
+			(_userManager, _signInManager) = (userManager, signInManager);
+
+		public async Task<Result<bool>> Login(string email, string password)
+		{
+			if (email is null or "")
+				return Result<bool>.Fail("O nome do usuário é obrigatório");
+			
+			if (password is null or "")
+				return Result<bool>.Fail("O password do usuário é obrigatório");
+
+			var result = await _signInManager.PasswordSignInAsync(email, password, false, true);
+
+			if (result.Succeeded)
+				return Result<bool>.Ok(true);
+
+			return Result<bool>.Fail("Não foi possível fazer o login");
+		}
+	}
+}
---
+++ src/Services/Identity.Application/Services/IAuthService.cs.cs
@@ -0,0 +1,11 @@
+﻿using Identity.Core;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Services
+{
+    public interface IAuthService
+    {
+        Task<Result<bool>> Login(string email, string password);
+        Task<Result<bool>> Register(string email, string password);
+    }
+}
