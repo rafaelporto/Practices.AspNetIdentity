b295fdf06a9d55124d607999acb530b813df9d1a
Rafael Monteiro Porto <rafael.monteiro@easynvest.com.br>
2020-11-17 11:20:04 -0300

    feat(Result): Implementando Result structure

--- src/Infrastructure/Identity.Core/Result.cs
+++ src/Infrastructure/Identity.Core/Result.cs
@@ -3,25 +3,22 @@ using System.Linq;
 
 namespace Identity.Core
 {
-	public record Result<T>
+	public readonly struct Result<T>
 	{
 		public bool IsSuccess => true;
 		public bool IsFailure => !IsSuccess;
-		public T Value { get; set; }
-		private List<string> _errors;
+		public T Value { get; init; }
+		public IReadOnlyList<string> Errors { get; init; }
 
-		public IReadOnlyList<string> Errors => _errors;
-
-
-		private Result(T value) => Value = value;
-		private Result(IEnumerable<string> errors) => _errors = errors.ToList();
-		private Result(string error) => _errors = new List<string>() { error };
+		private Result(T value) => (Value, Errors) = (value, new List<string>());
+		private Result(IEnumerable<string> errors) => (Value, Errors) = ( default, errors.ToList());
+		private Result(string error) => (Value, Errors) = (default, new List<string>() { error });
 
 		public static Result<T> Ok(T value) => new Result<T>(value);
 		public static Result<T> Fail(IEnumerable<string> errors) => new Result<T>(errors);
 		public static Result<T> Fail(string error) => new Result<T>(error);
 
 		public override string ToString() =>
-			$"IsSuccess: {IsSuccess}, Value: " + Value.ToString();
+			$"IsSuccess: {IsSuccess}, Value Type: {nameof(T)}, Value: {Value?.ToString()}";
 	}
 }
